fe6f8118bcdcec93ff0d92ec93ce59bc83aad9b3
DylanVanzan <dylanvanzan3001@gmail.com>
2017-11-08 17:44:18 +0100

    Consegna

---
+++ Risponditore/Server.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package server;
+
+import graph.Edge;
+import graph.Graph;
+import graph.GraphException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Utente
+ */
+public class Server {
+    
+    private static Graph grafo = new Graph();
+    
+     public static void main(String[] args) throws Exception {
+     System.out.println("Il ristorante è aperto!");
+
+        
+        grafo = server.Negozio.inizializeGraph();
+        try (ServerSocket listener = new ServerSocket(9898)) {
+            while (true) {
+                // crea il thread e lo lancia
+                new Cameriere(listener.accept()).start();
+            }
+        }
+     }
+     
+     private static class Cameriere extends Thread {
+
+        private final Socket socket;
+        private String clientName;
+
+        public Cameriere(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            String currentNode = "inizio";
+            String outStrm;
+            String succ;
+            PrintWriter out = null;
+            double costoTotale=0;
+            try {
+
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                out = new PrintWriter(socket.getOutputStream(), true);
+
+                out.println("Buona sera, come si chiama?");
+                clientName = in.readLine();
+                ArrayList adj = null;
+                double weight = 0;
+//                out.println("Buona sera " + clientName + ", cosa desidera? " + costoTotale);
+                while (!currentNode.equals("fine")) {
+                    outStrm = "";
+//                    out.println("Desidera altro? " + "(" + costoTotale + ")");
+                    out.println("Buona sera " + clientName + ", cosa desidera? " + costoTotale);
+                    try {
+                        adj = grafo.getAdjList(currentNode);
+                        outStrm += "   ";
+                        for (Object x : adj) {
+                            Edge app = (Edge) x;
+                            weight = grafo.getEdgeWeight(currentNode, app.getDest());
+                            costoTotale+=weight;
+                            if (weight > 0) {
+                                outStrm += app.getDest() + " ---> " + weight + "€";
+                            } else {
+                                outStrm += app.getDest() +  " *** ";
+                            }
+                        }
+                        outStrm+="   ";
+                        out.println(outStrm);
+                        succ = in.readLine().toLowerCase();
+                        currentNode = succ;
+                    } catch (GraphException e) {
+                        out.println("Errore opzione inesistente! Riprovare");
+                        currentNode = in.readLine();
+                    }
+                }
+            } catch (IOException e) {
+                myLog("Errore " + clientName + ": " + e);
+            } finally {
+                out.println("Arrivederci e grazie!");
+                try {
+                    socket.close();
+                } catch (IOException e) {
+
+                }
+                myLog("Connessioe col cliente " + clientName + " è chiusa");
+            }
+
+        }
+
+        private void myLog(String message) {
+            System.out.println(message);
+        }
+    }
+}
